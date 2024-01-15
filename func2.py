c7d9d9935a9ba7793748ad22da329dfb9918fde9
Hang <64747715+xx529@users.noreply.github.com>
2024-01-15 22:03:38 +0800

    add func2 to main

--- func2.py
+++ func2.py
@@ -1,2 +1,6 @@
 def func2():
     print('func2 in func2.py')
+
+
+def func2_adv():
+    func2()
--- main.py
+++ main.py
@@ -1,2 +1,5 @@
+from func2 import func2_adv
+
 if __name__ == '__main__':
+    func2_adv()
     print('PyCharm')
